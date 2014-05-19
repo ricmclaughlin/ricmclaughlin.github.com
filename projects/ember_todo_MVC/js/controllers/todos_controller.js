d2eaafb154a634eb994d174b82c911fdb3f90b75
ric mclaughlin <ric_mclaughlin@yahoo.com>
2014-05-19 14:35:20 +0200

    completed ember.js todoMVC app

--- projects/ember_todo_MVC/js/controllers/todos_controller.js
+++ projects/ember_todo_MVC/js/controllers/todos_controller.js
@@ -1,12 +1,6 @@
 Todos.TodosController = Ember.ArrayController.extend({
   actions: {
-    clearCompleted: function() {
-      var completed = this.filterBy('isCompleted', true);
-      completed.invoke('deleteRecord');
-      completed.invoke('save');
-    },
-
-    createTodo: function() {
+    createTodo: function () {
       // Get the todo title set by the "New Todo" text field
       var title = this.get('newTitle');
       if (!title.trim()) { return; }
@@ -22,23 +16,39 @@ Todos.TodosController = Ember.ArrayController.extend({
 
       // Save the new model
       todo.save();
+    },
+    clearCompleted: function () {
+      var completed = this.filterProperty('isCompleted', true);
+      completed.invoke('deleteRecord');
+      completed.invoke('save');
     }
   },
-  
-  hasCompleted: function() {
-      return this.get('completed') > 0;
-    }.property('completed'),
 
-  completed: function() {
-    return this.filterBy('isCompleted', true).get('length');
+  remaining: function () {
+    return this.filterProperty('isCompleted', false).get('length');
   }.property('@each.isCompleted'),
 
-  remaining: function() {
-    return this.filterBy('isCompleted', false).get('length');
-  }.property('@each.isCompleted'),
-
-  inflection: function() {
+  inflection: function () {
     var remaining = this.get('remaining');
-    return remaining === 1 ? 'todo' : 'todos';
-  }.property('remaining')
+    return remaining === 1 ? 'item' : 'items';
+  }.property('remaining'),
+
+  hasCompleted: function () {
+    return this.get('completed') > 0;
+  }.property('completed'),
+
+  completed: function () {
+    return this.filterProperty('isCompleted', true).get('length');
+  }.property('@each.isCompleted'),
+  
+  allAreDone: function (key, value) {
+    if (value === undefined) {
+      return !!this.get('length') && this.everyProperty('isCompleted', true);
+    } else {
+      this.setEach('isCompleted', value);
+      this.invoke('save')
+      return value;
+    }
+  }.property('@each.isCompleted')
 });
+
